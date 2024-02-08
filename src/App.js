092723dc3aa742f88b873b0d26c5d6d9fc400150
anthony20021 <156093526+anthony20021@users.noreply.github.com>
2024-02-08 11:04:34 +0100

    navBar

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const App = () => {
   return (
-    <h1 style={{textAlign: "center"}}>SuperBoutique</h1>
+    <h1 style={{textAlign: "center"}}>SuperMegaBoutique</h1>
   )
 }
 
