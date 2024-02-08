aed31c7177abdf4270b118f795a171a20c1aba21
Galdo <galdric.crabie1@hotmail.fr>
2024-02-08 12:31:02 +0100

    add ProductCard

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import Navbar from './components/NavBar'
 import '../src/css/app.css';
+import ProductCard from './components/ProductCard';
 
 const App = () => {
   return (
-    <Navbar></Navbar>
+    <>
+      <Navbar />
+      <ProductCard />
+    </>
   )
 }
 
