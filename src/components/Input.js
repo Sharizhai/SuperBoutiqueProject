657d5b57e6fc8008aa0100df69bb40a3fb19fa85
unknown <jade.besserve-moullahem@hotmail.fr>
2024-02-09 10:27:00 +0100

    register user

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,8 +1,9 @@
 const Input = ({name, type, placeholder, register, error}) => {
     return (
         <div style={{ display: "flex", flexDirection: "column"}}>
-            <input {...register(name)} type={type} placeholder={placeholder} />
-            {error && <span className="error-message">{error.message}</span>}
+            <input {...register(name, { required: true })} type={type} placeholder={placeholder}/>
+            {error && error.type === 'required' && <span className="error-message">Ce champ est requis.</span>}
+            {error && error.type !== 'required' && <span className="error-message">{error.message}</span>}
         </div>
     );
 };
--- src/routing/RegisterPage.js
+++ src/routing/RegisterPage.js
@@ -1,18 +1,39 @@
 import {Link} from 'react-router-dom';
 import {useForm} from "react-hook-form";
-import { useState } from 'react';
 import Button from "../components/Button";
 import Input from '../components/Input';
+import wretch from "wretch";
 
 const Register = () => {
 
     const { register, handleSubmit, reset, formState: { errors } } = useForm();
-    const [users, setUsers] = useState([]);
-    console.log(errors);
 
     const onSubmit = (data) => {
-        setUsers(prevUsers => [...prevUsers, data]);
-        reset();
+        console.log(data);
+
+        if(data.email === data.mailRepeat) {
+
+            let user={
+                nom: data.lastName,
+                prenom: data.firstName,
+                age: data.age,
+                address:{
+                    ville: data.city,
+                    postalCode: data.zipCode,
+                    numeroRue: data.streetNumber,
+                    nomRue: data.street,
+                },
+                email: data.email,
+                password: data.password,
+            }
+    
+            wretch().post(user, "https://api-3wa-ecomm-524fde41edfa.herokuapp.com/api/signup");
+            reset();
+        }
+
+        else(
+            alert("Le second mail est différent du premier. Veuillez vérifier votre saisie !")
+        );
     };
 
     return (
@@ -24,39 +45,22 @@ const Register = () => {
                     <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
                         <div style={{ marginTop: "30px", display: "flex", flexDirection: "column", gap: "15px" }}>
                             <h3 style={{ textAlign: "center", marginBottom: "15px", color: "#1A5359" }}>Informations d'authentification</h3>
-                            <Input name="mail" type="text" placeholder="E-mail" register={register} error={errors.mail} />
-                            
-                            <input {...register("mailRepeat", { required: true })} type="text" placeholder="Confirmez votre e-mail"/>
-                            {errors.mailRepeat && <span className="error-message">Ce champ est requis.</span>}
-                            
-                            <input {...register("password", { required: true })} type="password" placeholder="Mot de passe"/>
-                            {errors.password && <span className="error-message">Ce champ est requis.</span>}
+                            <Input name="email" type="email" placeholder="E-mail" register={register} error={errors.email} />
+                            <Input name="mailRepeat" type="email" placeholder="Confirmez votre e-mail" register={register} error={errors.mailRepeat} />
+                            <Input name="password" type="password" placeholder="Mot de passe" register={register} error={errors.password} />
                         </div>
 
                         <div style={{ marginTop: "50px", display: "flex", flexDirection: "column", gap: "15px" }}>
                             <h3 style={{ textAlign: "center", marginBottom: "15px", color: "#1A5359" }}>Informations personnelles</h3>
-                            <input {...register("lastName", { required: true })} type="text" placeholder="Nom"/>
-                            {errors.lastName && <span className="error-message">Ce champ est requis.</span>}
-                            
-                            <input {...register("firstName", { required: true })} type="text" placeholder="Prénom"/>
-                            {errors.firstName && <span className="error-message">Ce champ est requis.</span>}
-                            
-                            <input {...register("age", { required: true, pattern: /^\d+$/ })} type="number" placeholder="Age"/>
-                            {errors.age && errors.age.type === "required" && <span className="error-message">Ce champ est requis.</span>}
-                            {errors.age && errors.age.type === "pattern" && <span className="error-message">L'âge doit être un nombre.</span>}
+                            <Input name="lastName" type="text" placeholder="Nom" register={register} error={errors.lastName} />
+                            <Input name="firstName" type="text" placeholder="Prénom" register={register} error={errors.firstName} />
+                            <Input name="age" type="number" placeholder="Age" register={register} error={errors.age} />
                             
                             <p style={{marginBottom : "0px", marginTop : "12px", color: "#1A5359"}}>Adresse de livraison :</p>
-                            <input {...register("streetNumber", { required: true })} type="text" placeholder="Numéro de rue"/>
-                            {errors.streetNumber && <span className="error-message">Ce champ est requis.</span>}
-                            
-                            <input {...register("street", { required: true })} type="text" placeholder="Rue"/>
-                            {errors.street && <span className="error-message">Ce champ est requis.</span>}
-                            
-                            <input {...register("zipCode", { required: true })} type="text" placeholder={errors.zipCode ? "Ce champ est requis." : "Code postal"}/>
-                            {errors.zipCode && <span className="error-message">Ce champ est requis.</span>}
-                            
-                            <input {...register("city", { required: true })} type="text" placeholder="Ville"/>
-                            {errors.city && <span className="error-message">Ce champ est requis.</span>}
+                            <Input name="streetNumber" type="text" placeholder="Numéro de rue" register={register} error={errors.streetNumber} />
+                            <Input name="street" type="text" placeholder="Rue" register={register} error={errors.street} />
+                            <Input name="zipCode" type="text" placeholder="Code postal" register={register} error={errors.zipCode} />
+                            <Input name="city" type="text" placeholder="Ville" register={register} error={errors.city} />
                         </div>
 
                         <div style={{ marginTop: "40px" }}>
